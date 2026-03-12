171c8ae0990fb7f4481012d0fd10f8fe1f860e59
Haritham002 <haritham002@gmail.com>
2026-03-12 22:21:01 +0530

    Added 3 Arrayprograms

---
+++ ReverseArray.java
@@ -0,0 +1,8 @@
+class reverseArray{
+    public static void main(String[] args){
+        char[] ch={'a','b','c','d','e'};
+        for(int i=ch.length-1;i>=0;i--){
+            System.out.println(ch[i]);
+        }
+    }
+}
